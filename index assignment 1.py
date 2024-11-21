063ffb360122e07a3345885ceb029789ce795868
Zimkhitha23 <zimmkonjeni@gmail.com>
2024-11-21 17:28:05 +0200

    Update index assignment 1.py

--- index assignment 1.py
+++ index assignment 1.py
@@ -3,6 +3,5 @@ num1 = float(input("Enter Number 1:"))
 num2 = float(input("Enter Number 2:"))
 sum = num1 + num2
 
-print ssignment 1
-s (f"The sum of {num1} and {num2} is {sum}.")
+print (f"The sum of {num1} and {num2} is {sum}.")
 
